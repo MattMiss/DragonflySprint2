f5811a51b4b2bfa27bf6fd445b56d7a68dd940e7
Matthew Miss <matt.w.miss@gmail.com>
2024-03-06 12:07:22 -0800

    Added functionality to save DarkMode preference with cookies. The localstorage darkmode methods were left in and commented out so either can be used.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,78 @@
 let darkModeSwitch;
-let footerJobsSection;
-let footerResourcesSection;
+
 
 window.addEventListener('load', function() {
     //console.log("Window loaded");
-    setupDarkMode();
+    console.log(document.cookie);
+
+    // Setup Dark Mode using Cookies
+    setupCookieDarkMode();
+
+    // Setup Dark Mode using localStorage
+    //setupLSDarkMode();
 });
 
-function setupDarkMode(){
+
+
+function setupCookieDarkMode(){
     darkModeSwitch = document.getElementById("dark-mode-switch");
+    darkModeSwitch.addEventListener("change", () => {
+        theme === 'light' ? setCookieDarkMode('dark') : setCookieDarkMode('light');
+    });
+
     if (theme !== null){
-        setDarkMode(theme);
+        setCookieDarkMode(theme);
         darkModeSwitch.checked = theme === 'dark';
     }else
     {
-        localStorage.setItem('theme','light');
+        setCookieItem('theme', 'light');
+    }
+
+
+}
+
+function setCookieDarkMode(newTheme){
+    //console.log("Changed to : " + newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+    setCookieItem('theme', newTheme);
+    theme = newTheme;
+}
+
+// Sets the cookie item at the max value (400 days). If user visits website within the 400 days, the cookie
+// expiration will be extended by another 400 days and the user preferences will persist
+// If user waits longer than 400 days, the cookie will be set back to default or null
+function setCookieItem(key, value){
+    const dt = new Date();
+    dt.setTime(dt.getTime() + (400 * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + dt.toString();
+    document.cookie = key + "=" + value + ";" + expires + ";path=/";
+    console.log(document.cookie);
+}
+
+/*
+function setupLSDarkMode(){
+    darkModeSwitch = document.getElementById("dark-mode-switch");
+    if (theme !== null){
+        setLSDarkMode(theme);
+        darkModeSwitch.checked = theme === 'dark';
+    }else
+    {
+        setLocalStorageItem('theme', 'light');
     }
 
     darkModeSwitch.addEventListener("change", () => {
-        theme === 'light' ? setDarkMode('dark') : setDarkMode('light');
+        theme === 'light' ? setLSDarkMode('dark') : setLSDarkMode('light');
     });
 }
 
-function setDarkMode(newTheme){
+function setLSDarkMode(newTheme){
     //console.log("Changed to : " + newTheme);
     document.documentElement.setAttribute('data-theme', newTheme);
-    localStorage.setItem('theme', newTheme);
+    setLocalStorageItem('theme', newTheme);
     theme = newTheme;
 }
+
+function setLocalStorageItem(key, value){
+    localStorage.setItem(key, value);
+}
+*/
