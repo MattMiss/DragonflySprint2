af93728294bd62fc7f653228271fda9a0fb8e35f
Yadira Cervantes <githubusername@noreply.github.com>
2024-03-19 22:27:54 -0700

    removed visibility attribute from contact_form.php, contactscript.js now removes whitespaces

--- js/contactscript.js
+++ js/contactscript.js
@@ -1,15 +1,18 @@
-// contact form
+// constants
 const MIN_MESSAGE = 25;
 const MAX_MESSAGE = 1000;
+
+// elements
 const firstName = document.getElementById("contact-first-name");
 const lastName = document.getElementById("contact-last-name")
 const email = document.getElementById("contact-email");
 const message = document.getElementById("contact-message");
 
+// calls all validate functions
 function validateForm(){
-    let isNameValid = validateName() === true;
-    let isEmailValid = validateEmail() === true;
-    let isMessageValid = validateMessage() === true;
+    let isNameValid = validateName();
+    let isEmailValid = validateEmail();
+    let isMessageValid = validateMessage();
 
     let isValid = isNameValid && isEmailValid && isMessageValid;
 
@@ -18,18 +21,19 @@ function validateForm(){
 
 // validates first and last name
 function validateName() {
-    let isFirstValid = firstName.value.length !== 0;
-    let isLastValid = lastName.value.length !== 0;
+    // removes whitespaces and checks length
+    let isFirstValid = firstName.value.trim().length !== 0;
+    let isLastValid = lastName.value.length.trim().length !== 0;
     let isValid = isFirstValid === true && isLastValid === true;
 
-    // selects message if it exists
+    // gets error message
     const nameError = document.getElementById("contact-name-error");
     const nameMessage = "Please enter your first and last name";
     const firstNameMessage = "Please enter your first name";
     const lastNameMessage = "Please enter your last name";
 
 
-    // decides which message to display (if any)
+    // decides which message to display (if any), adds form-input-error class
     if(isValid === true) {
         nameError.style.visibility = "hidden";
         firstName.classList.remove("form-input-error");
@@ -57,17 +61,19 @@ function validateName() {
 
 // validates email
 function validateEmail() {
+    // tests email against a regular expression
     let re = /[^\s@]+@[^\s@]+\.[^\s@]+/;
     let isValid = re.test(email.value);
 
     const emailError = document.getElementById("contact-email-error");
 
-    if(isValid === true) {
-        emailError.style.visibility = "hidden";
-        email.classList.remove("form-input-error");
-    } else {
+    // displays message, adds form-input-error class
+    if(isValid === false) {
         emailError.style.visibility = "visible";
         email.classList.add("form-input-error");
+    } else {
+        emailError.style.visibility = "hidden";
+        email.classList.remove("form-input-error");
     }
 
     return isValid;
@@ -75,9 +81,12 @@ function validateEmail() {
 
 // validates message
 function validateMessage() {
-    let isValid = (message.value.length >= MIN_MESSAGE) && (message.value.length <= MAX_MESSAGE);
+    // removes whitespaces and checks length
+    let isValid = (message.value.trim().length >= MIN_MESSAGE) && (message.value.trim().length <= MAX_MESSAGE);
+
     let messageError = document.getElementById("contact-message-error");
 
+    // displays message, adds form-input-error class
     if(isValid === false) {
         messageError.style.visibility = "visible";
         message.classList.add("form-input-error");
