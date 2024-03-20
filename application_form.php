9c851449b58f8432fe8117e9154f3202735c3b68
Yadira Cervantes <githubusername@noreply.github.com>
2024-03-20 00:04:20 -0700

    edited application form validation

--- application_form.php
+++ application_form.php
@@ -21,38 +21,37 @@ include 'php/nav_bar.php'
 <main>
     <div class="container p-3" id="main-container">
         <h3 class="form-header">Application Form</h3>
-        <!--        <h2 class="pt-3">Application Form</h2>-->
         <div class="form-container">
             <form method="post" action="php/application_submit.php" onsubmit="return validateForm()" class="form-body my-3">
                 <div class="mb-4">
                     <label for="job-name" class="form-label">Job Name*</label>
-                    <small id="app-job-name-error" class="warning" style="visibility: hidden"></small>
+                    <small id="app-job-name-error" class="warning"></small>
                     <input type="text" class="form-control" id="job-name" name="job-name" maxlength="60">
                 </div>
                 <div class="mb-4">
                     <label for="employer-name" class="form-label">Employer Name*</label>
-                    <small id="app-employer-error" class="warning" style="visibility: hidden"></small>
+                    <small id="app-employer-error" class="warning"></small>
                     <input type="text" class="form-control" id="employer-name" name="employer-name" maxlength="60">
                 </div>
                 <div class="mb-4">
                     <label for="job-url" class="form-label">Job Description URL*</label>
-                    <small id="app-url-error" class="warning" style="visibility: hidden"></small>
-                    <input type="text" class="form-control" id="job-url" name="job-url" maxlength="500" required>
+                    <small id="app-url-error" class="warning"></small>
+                    <input type="text" class="form-control" id="job-url" name="job-url" maxlength="500">
                 </div>
                 <div class="mb-4">
                     <label for="job-description" class="form-label">Job Description</label>
                     <textarea class="form-control" id="job-description" name="job-description"
-                              placeholder="Little summary of the role of the job..." maxlength="500" rows="3" required></textarea>
+                              placeholder="Summary of the job..." maxlength="500" rows="3"></textarea>
                 </div>
                 <div class="mb-4">
                     <label for="today" class="form-label">Date of Application*</label>
-<!--                    <small id="app-date-error" class="warning" style="visibility: hidden"></small>-->
-                    <input type="date" class="form-control" id="today" name="app-date" required>
+                    <small id="app-date-error" class="warning"></small>
+                    <input type="date" class="form-control" id="today" name="app-date">
                 </div>
                 <div class="mb-4">
                     <label for="application-status" class="form-label mb-3">Application Status*</label><br>
-<!--                    <div id="app-status-error" class="warning" style="visibility: hidden">Please select a status</div>-->
-                    <select name="application-status" id="application-status" required>
+                    <div id="app-status-error" class="warning">Please select a status</div>
+                    <select name="application-status" id="application-status">
                         <option value="select">Select an option</option>
                         <option value="need-to-apply">Need to apply</option>
                         <option value="applied">Applied</option>
@@ -61,7 +60,6 @@ include 'php/nav_bar.php'
                         <option value="accepted">Accepted</option>
                         <option value="inactive">Inactive/Expired</option>
                     </select>
-                    <div id="application-wrong" style="color:red"></div>
                 </div>
                 <div class="mb-4">
                     <label for="follow-updates" class="form-label">Updates</label>
@@ -71,8 +69,8 @@ include 'php/nav_bar.php'
 
                 <div class="mb-4">
                     <label for="two-weeks" class="form-label">Follow up date*</label>
-<!--                    <div id="app-followup-error" class="warning" style="visibility: hidden"></div>-->
-                    <input type="date" class="form-control" id="two-weeks" name="followup-date" required>
+                    <div id="app-followup-error" class="warning"></div>
+                    <input type="date" class="form-control" id="two-weeks" name="followup-date">
                 </div>
 
                 <button type="submit" class="submit-btn">Submit</button>
--- js/applicationscript.js
+++ js/applicationscript.js
@@ -1,15 +1,15 @@
-// const status = document.getElementById("application-status");
-// const status = document.getElementsByTagName("option");
-const submit = document.getElementById("submit-btn");
+// constants
+const STATUS_VALUES = ["select", "need-to-apply", "applied", "interviewing", "rejected", "accepted", "inactive"];
+const MAX_CHARACTERS = 60;
+const MAX_URL = 500;
+
+// elements
 const jobName = document.getElementById("job-name");
 const employer = document.getElementById("employer-name");
 const jobURL = document.getElementById("job-url");
-// const appDate = document.getElementById("app-date");
-// const followupDate = document.getElementById("two-weeks");
-//
-// const STATUS_VALUES= ["select", "need-to-apply", "applied", "interviewing", "rejected", "accepted", "inactive"];
-const MAX_CHARACTERS = 60;
-// const MAX_URL = 500;
+const appDate = document.getElementById("today");
+const status = document.getElementById("application-status");
+const followupDate = document.getElementById("two-weeks");
 
 window.addEventListener('load', function() {
     setTodayDate();
@@ -19,114 +19,139 @@ window.addEventListener('load', function() {
 function validateForm() {
     let isJobNameValid = validateJobName();
     let isEmployerValid = validateEmployer()
+    let isURLValid = validateJobURL();
+    let isAppDateValid = validateAppDate();
+    let isFollowupValid = validateFollowupDate();
+    let isStatusValid = validateStatus();
 
-    let isValid = isJobNameValid && isEmployerValid;
+    let isValid = isJobNameValid && isEmployerValid && isURLValid && isAppDateValid && isFollowupValid && isStatusValid;
 
     return isValid;
 }
 
 function validateJobName() {
-    let isValidLength = jobName.value.length <= MAX_CHARACTERS;
-    let isNotEmpty = jobName.value.length > 0;
+    let isValidLength = jobName.value.trim().length <= MAX_CHARACTERS;
+    let isNotEmpty = jobName.value.trim().length > 0;
     let isValid = isValidLength && isNotEmpty;
-    let jobNameError = document.getElementById("app-job-name-error");
 
-    if(isNotEmpty === false) {
-        jobNameError.innerText = "Please enter a job name";
-        jobNameError.style.visibility = "visible";
-        jobName.classList.add("form-input-error");
-    } else {
-        jobNameError.style.visibility = "hidden";
+    let jobNameError = document.getElementById("app-job-name-error");
+    let message = `Please enter between 0 and ${MAX_CHARACTERS} characters`;
+    let emptyMessage = "Please enter a job name";
+
+    if(isValid === true) {
         jobName.classList.remove("form-input-error");
+        jobNameError.style.visibility = "hidden";
+    } else {
+        if((isNotEmpty === false && isValidLength === false) || isValid === false) {
+            jobNameError.innerText = message;
+        } else if(isNotEmpty === false) {
+            jobNameError.innerText = emptyMessage;
+        }
+
+        jobName.classList.add("form-input-error")
+        jobNameError.style.visibility = "visible";
     }
 
-    return isNotEmpty;
+    return isValid;
 }
 
 function validateEmployer() {
-    let isValidLength = employer.value.length <= MAX_CHARACTERS;
-    let isNotEmpty = employer.value.length > 0;
-    console.log(employer.value.length > 0);
+    let isValidLength = employer.value.trim().length <= MAX_CHARACTERS;
+    let isNotEmpty = employer.value.trim().length > 0;
     let isValid = isValidLength && isNotEmpty;
-    let employerError = document.getElementById("app-employer-error");
 
-    if(isNotEmpty === false) {
-        employerError.innerText = "Please enter an employer name";
-        employerError.style.visibility = "visible";
-        employer.classList.add("form-input-error")
-    } else {
-        employerError.style.visibility = "hidden";
+    let employerError = document.getElementById("app-employer-error");
+    let message = `Please enter between 0 and ${MAX_URL} characters`;
+    let emptyMessage = "Please enter an employer";
+
+    if(isValid === true) {
         employer.classList.remove("form-input-error");
+        employerError.style.visibility = "hidden";
+    } else {
+        if((isNotEmpty === false && isValidLength === false) || isValid === false) {
+            employerError.innerText = message;
+        } else if(isNotEmpty === false) {
+            employerError.innerText = emptyMessage;
+        }
+
+        employer.classList.add("form-input-error")
+        employerError.style.visibility = "visible";
     }
 
     return isNotEmpty;
 }
 
+function validateJobURL() {
+    let re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+    let isValidLength = jobURL.value.length <= MAX_URL;
+    let isValidFormat = re.test(jobURL.value);
+    let isValid = isValidLength && isValidFormat;
 
-// function validateJobURL() {
-//     let re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
-//     let isValidLength = jobURL.value.length <= MAX_URL;
-//     let isValidFormat = re.test(jobURL.value);
-//     let isValid = isValidLength && isValidFormat;
-//     let urlError = document.getElementById("app-url-error");
-//
-//     if(isValid === false) {
-//         jobNameError.innerText = "Must be 500 characters or less";
-//         jobNameError.style.visibility = "visible";
-//     } else {
-//         jobNameError.style.visibility = "hidden";
-//     }
-//
-//     if(isValidFormat === false) {
-//         urlError.innerText = "Please enter a valid url";
-//         urlError.style.visibility = "visible";
-//         jobURL.classList.add('form-input-error');
-//
-//     } else {
-//         urlError.style.visibility = "hidden";
-//         jobURL.classList.remove('form-input-error');
-//     }
-//
-//     return isValidFormat;
-// }
+    let urlError = document.getElementById("app-url-error");
+    let message = `Please enter a valid URL less than ${MAX_URL} characters`;
+    let formatMessage = "Please enter a valid URL";
+    let lengthMessage = "Please enter a URL less than 500 characters";
 
-// function validateDate(date) {
-//     let isValid = date !== null;
-//     let jobNameError = document.getElementById("app-date-error");
-//
-//     if(isValid === false) {
-//         jobNameError.innerText = "Please select a date";
-//         jobNameError.style.visibility = "visible";
-//     } else {
-//         jobNameError.style.visibility = "hidden";
-//     }
-//
-//     return isValid;
-// }
+    if(isValid === true) {
+        jobURL.classList.remove("form-input-error");
+        urlError.style.visibility = "hidden";
+    } else {
+        if(isValidFormat === false && isValidLength === false) {
+            urlError.innerText = message;
+        } else if(isValidFormat === false) {
+            urlError.innerText = formatMessage;
+        } else if(isValidLength === false) {
+            urlError.innerText = lengthMessage;
+        }
 
-// function validateStatus() {
-//     let isValid = true;
-//     let isValidStatus = false;
-//     let checked = 0;
-//
-//     for(let i = 0; i < status.length; i++) {
-//         if(status[i].selected === true) {
-//             isValidStatus = STATUS_VALUES.includes(status[i].value);
-//             checked++;
-//         }
-//     }
-//
-//     isValid = isValidStatus === true && checked === 1;
-//     const statusError = document.getElementById("app-status-error");
-//
-//     if(isValid === false) {
-//         statusError.style.visibility = "visible";
-//     } else {
-//         statusError.style.visibility = "hidden";
-//     }
-//
-//     return isValid;
-// }
+        jobURL.classList.add("form-input-error")
+        urlError.style.visibility = "visible";
+    }
+
+    return isValidFormat;
+}
+
+function validateAppDate() {
+    let isValid = appDate.value !== null;
+    let appDateError = document.getElementById("app-date-error");
+
+    if(isValid === false) {
+        appDateError.innerText = "Please select a date";
+        appDateError.style.visibility = "visible";
+    } else {
+        appDateError.style.visibility = "hidden";
+    }
+
+    return isValid;
+}
+
+function validateFollowupDate() {
+    let isValid = followupDate.value !== null;
+    let appDateError = document.getElementById("app-followup-error");
+
+    if(isValid === false) {
+        appDateError.innerText = "Please select a date";
+        appDateError.style.visibility = "visible";
+    } else {
+        appDateError.style.visibility = "hidden";
+    }
+
+    return isValid;
+}
+
+function validateStatus() {
+    let isValid = STATUS_VALUES.includes(status.value);
+
+    const statusError = document.getElementById("app-status-error");
+
+    if(isValid === false) {
+        statusError.style.visibility = "visible";
+    } else {
+        statusError.style.visibility = "hidden";
+    }
+
+    return isValid;
+}
 
 function setTodayDate() {
     var today = new Date();
